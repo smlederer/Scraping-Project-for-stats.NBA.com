bcb5dacb8091840c711356c0df2a0ef37c620f1c
smlederer <34457857+smlederer@users.noreply.github.com>
2019-05-05 22:29:55 -0400

    Update ReadFolderJson.R

--- ReadFolderJson.R
+++ ReadFolderJson.R
@@ -1,6 +1,15 @@
 library(jsonlite)
 library(tidyverse)
 
+NBAjsonToDF = function(json_path){
+  library(jsonlite)
+  data = jsonlite::fromJSON(json_path)
+  df = as.data.frame(data$resultSets$rowSet)
+  names(df) = unlist(data$resultSets$headers)
+  return(df)
+}
+
+
 dir = "C:/Users/Sam/Desktop/NBA Data/"
 
 for (name in list.files(dir)){
